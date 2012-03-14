e8a8a5c9f7f78c861738ea3483fe823abb3aad76
Érico <https://pt.wikipedia.org/wiki/User:rico>
2012-03-14 20:14:46 -0300

    Allow usage on pages containing Template:PE-header

--- src/PagesForDeletion.js
+++ src/PagesForDeletion.js
@@ -1,4 +1,8 @@
 // [[Special:GlobalUsage/User:Helder.wiki/Tools/PagesForDeletion.js]] ([[File:User:Helder.wiki/Tools/PagesForDeletion.js]])
-if( mw.config.get('wgPageName') === 'Wikipédia:Páginas_para_eliminar/Lista' && $.inArray( mw.config.get('wgAction'), ['view', 'purge'] ) !== -1 ){
-    mw.loader.load( '//pt.wikibooks.org/w/index.php?title=User:Helder.wiki/Tools/PagesForDeletion.js&action=raw&ctype=text/javascript&smaxage=21600&maxage=86400' );
+if( $.inArray( mw.config.get('wgAction'), ['view', 'purge'] ) !== -1 ){
+        $(function(){
+                if( $('#pe-header').length !== 0 ){
+                        mw.loader.load( '//pt.wikibooks.org/w/index.php?title=User:Helder.wiki/Tools/PagesForDeletion.js&action=raw&ctype=text/javascript&smaxage=21600&maxage=86400' );
+                }
+        });
 }
