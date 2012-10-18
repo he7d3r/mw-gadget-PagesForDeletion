ae9e356814fa88f0e8a2f27b0c712f47cba4eb5a
Helder <he7d3r@gmail.com>
2012-10-18 12:53:33 -0300

    Optimization
    Now that the script was moved to a module, we can load its minified version:
    pt.wikipedia.org/w/load.php?modules=ext.gadget.PagesForDeletionCore&only=scripts
    generated by MediaWiki's Resource Loader, by using
    mw.loader.load( 'ext.gadget.PagesForDeletion')

--- src/PagesForDeletion.js
+++ src/PagesForDeletion.js
@@ -1,8 +1,12 @@
-// [[Special:GlobalUsage/User:Helder.wiki/Tools/PagesForDeletion.js]] ([[File:User:Helder.wiki/Tools/PagesForDeletion.js]])
+/*jslint white: true */
+/*global mediaWiki, jQuery */
+( function ( mw, $ ) {
+'use strict';
 if( $.inArray( mw.config.get('wgAction'), ['view', 'purge'] ) !== -1 ){
         $(function(){
                 if( $('#pe-header').length !== 0 ){
-                        mw.loader.load( '//pt.wikibooks.org/w/index.php?title=User:Helder.wiki/Tools/PagesForDeletion.js&action=raw&ctype=text/javascript&smaxage=21600&maxage=86400' );
+                        mw.loader.load( 'ext.gadget.PagesForDeletionCore' );
                 }
         });
 }
+}( mediaWiki, jQuery ) );
