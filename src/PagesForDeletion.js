45cb57b2d46c9093bfa790512a567a3679dd62de
Helder <he7d3r@gmail.com>
2012-10-20 03:25:39 -0300

    Use importScript to workaround a MediaWiki bug
    MediaWiki was returning an empty script for
    bits.wikimedia.org/pt.wikipedia.org/load.php?debug=true&lang=pt-br&modules=ext.gadget.PagesForDeletionCore&only=scripts&skin=vector&version=20121018T124359Z
    
    See [T42288](https://phabricator.wikimedia.org/T42288) for details.

--- src/PagesForDeletion.js
+++ src/PagesForDeletion.js
@@ -5,7 +5,7 @@
 if( $.inArray( mw.config.get('wgAction'), ['view', 'purge'] ) !== -1 ){
         $(function(){
                 if( $('#pe-header').length !== 0 ){
-                        mw.loader.load( 'ext.gadget.PagesForDeletionCore' );
+                        importScript( 'MediaWiki:Gadget-PagesForDeletion.js/Core.js' );
                 }
         });
 }
