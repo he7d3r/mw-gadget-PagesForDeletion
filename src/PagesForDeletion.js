ec5bfac286194c082af04dfbe5b83cf7ddb46649
Helder <he7d3r@gmail.com>
2014-09-28 16:57:32 -0300

    Apply code convetions for JS (checked by jscs, using config from (https://github.com/wikimedia/mediawiki-core/blob/master/.jscsrc)

--- src/PagesForDeletion.js
+++ src/PagesForDeletion.js
@@ -2,9 +2,9 @@
 /*global mediaWiki, jQuery */
 ( function ( mw, $ ) {
 'use strict';
-if( $.inArray( mw.config.get('wgAction'), ['view', 'purge'] ) !== -1 ){
-        $(function(){
-                if( $('#pe-header').length !== 0 ){
+if ( $.inArray( mw.config.get('wgAction'), ['view', 'purge'] ) !== -1 ) {
+        $(function () {
+                if ( $('#pe-header').length !== 0 ) {
                         mw.loader.load( 'ext.gadget.PagesForDeletionCore' );
                 }
         });
