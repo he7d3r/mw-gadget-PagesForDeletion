f882ceac03292ed00d2f5eb99a42f81f3dd92dc5
Helder <he7d3r@gmail.com>
2015-03-27 13:03:56 -0300

    Update comment

--- src/PagesForDeletion.js
+++ src/PagesForDeletion.js
@@ -1,5 +1,11 @@
+// [[File:User:He7d3r/Tools/PagesForDeletion.js]] (workaround for [[phab:T35355]])
 /*jslint white: true */
 /*global mediaWiki, jQuery */
+/**
+ * Loader for the PagesForDeletion gadget
+ * @author: Helder (https://github.com/he7d3r)
+ * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
+ */
 ( function ( mw, $ ) {
 'use strict';
 if ( $.inArray( mw.config.get('wgAction'), ['view', 'purge'] ) !== -1 ) {
